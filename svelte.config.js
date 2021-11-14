f06691867fbcf501ebb5dc6a9a109a93c7672cc2
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2021-11-14 10:04:09 -0500

    Svelte config update

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,6 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: sveltePreprocess({
-		defaults: {
-			style: 'postcss'
-		},
 		postcss: true
 	}),
 
