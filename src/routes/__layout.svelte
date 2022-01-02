f69e443eb2d2888498f7bf8494bb357cdc8e1175
JoshuaPelealu <hasianjoshua@gmail.com>
2022-01-02 10:16:50 -0500

    Updates to Tailwind and Welcome Template

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,14 +5,9 @@
 	@tailwind components;
 	@tailwind utilities;
 
-	@import url('https://fonts.googleapis.com/css2?family=Comic+Neue:wght@700&display=swap');
 	* {
-		@apply text-prussian;
 		padding: 0;
 		margin: 0;
 	}
 
-	:global(html) {
-		background-color: #E5E5E5;
-	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,19 @@
     let dark = false;
 </script>
 
-<div></div>
+<div class="flex flex-col justify-around items-center">
+    <span class="text-9xl font-bold mb-10"
+        >Hello <br /><span class="text-orange-300">SvelteKit</span><br />
+        <span class="text-blue-500">Tailwind</span>!</span
+    >
+    <span>
+        Click <a href="https://kit.svelte.dev" target="_blank" class="text-blue-600 underline hover:text-blue-800"
+            >here</a
+        > to learn more about SvelteKit
+    </span>
+    <span
+        >Click <a href="https://tailwindcss.com" target="_blank" class="text-blue-600 underline hover:text-blue-800"
+            >here</a
+        > to learn more about Tailwind</span
+    >
+</div>
