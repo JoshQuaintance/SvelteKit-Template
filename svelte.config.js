0ae013bb4b3dcbf41b1ccecf5060fcf99fa83e8b
JoshQuaintance <hasianjoshua@gmail.com>
2022-08-26 18:58:23 -0400

    Remove kit.target and added tsconfig to point to the sveltekit one

--- svelte.config.js
+++ svelte.config.js
@@ -10,10 +10,7 @@ const config = {
         })
     ],
 
-    kit: {
-        // hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte'
-    }
+    kit: {}
 };
 
 export default config;
